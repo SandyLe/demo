75bd12f6cddb8fc478e4f342459b93ca4e31fb9e
lixianting <li_xianting@163.com>
2018-11-23 09:12:30 +0800

    集成Shiro

---
+++ demo-server/src/main/java/com/sl/demo/server/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.sl.demo.server.controller;
+
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(value = "LoginController", description = "登录接口")
+public class LoginController {
+
+}
