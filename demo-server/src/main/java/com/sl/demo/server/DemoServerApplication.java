9e0bc73700029608d9fd83741c3780a33422e086
lixianting <li_xianting@163.com>
2019-04-30 18:12:37 +0800

    测试服务调用

--- demo-server/src/main/java/com/sl/demo/server/DemoServerApplication.java
+++ demo-server/src/main/java/com/sl/demo/server/DemoServerApplication.java
@@ -3,16 +3,53 @@ package com.sl.demo.server;
 import com.sl.demo.core.configuration.CoreConfig;
 import com.sl.demo.core.configuration.ShiroConfig;
 import com.sl.demo.core.configuration.Swagger2;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ImportAutoConfiguration({Swagger2.class, CoreConfig.class, ShiroConfig.class})
 @EntityScan(basePackages = {"com.sl.domain.entity"})
 public class DemoServerApplication {
 
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        //springboot版本不同可能下面的类名会不同或者类的包路径会不同
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        //监听http的端口号
+        connector.setPort(80);
+        connector.setSecure(false);
+        //监听到http的端口号后转向到的https的端口号
+        System.out.println("监听到了80端口");
+        connector.setRedirectPort(443);//这里的端口写成和配置文件一样的端口就Ok
+        return connector;
+    }
     public static void main(String[] args) {
         SpringApplication.run(DemoServerApplication.class, args);
     }
--- demo-server/src/main/java/com/sl/demo/server/common/WechatMenuInit.java
+++ demo-server/src/main/java/com/sl/demo/server/common/WechatMenuInit.java
@@ -1,95 +1,91 @@
-package com.sl.demo.server.common;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.sl.domain.dto.wechat.WechatButton;
-import com.sl.domain.dto.wechat.WechatClickButton;
-import com.sl.domain.dto.wechat.WechatMenu;
-import com.sl.domain.dto.wechat.WechatViewButton;
-import org.apache.http.client.ClientProtocolException;
-
-import java.io.IOException;
-
-public class WechatMenuInit {
-
-    public static final String CREATE_MENU_URL = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
-
-    public static WechatMenu initMenu(){
-        WechatMenu menu = new WechatMenu();
-        WechatClickButton clickButton11 = new WechatClickButton();
-        clickButton11.setKey("11");
-        clickButton11.setName("关于我们");
-        clickButton11.setType("click");
-
-        WechatClickButton clickButton12 = new WechatClickButton();
-        clickButton12.setKey("12");
-        clickButton12.setName("加入我们");
-        clickButton12.setType("click");
-
-        WechatViewButton viewButton21 = new WechatViewButton();
-        viewButton21.setUrl("http://www.qifou.net/test/test");
-        viewButton21.setName("网站系统");
-        viewButton21.setType("view");
-
-        WechatViewButton viewButton22 = new WechatViewButton();
-        viewButton22.setUrl("http://www.qifou.net/test/test");
-        viewButton22.setName("企业ERP");
-        viewButton22.setType("view");
-
-        WechatViewButton viewButton31 = new WechatViewButton();
-        viewButton31.setUrl("http://www.qifou.net/test/test");
-        viewButton31.setName("APP");
-        viewButton31.setType("view");
-
-        WechatViewButton viewButton32 = new WechatViewButton();
-        viewButton32.setUrl("http://www.qifou.net/test/test");
-        viewButton32.setName("微信小程序");
-        viewButton32.setType("view");
-
-        WechatClickButton clickButton31 = new WechatClickButton();
-        clickButton31.setKey("31");
-        clickButton31.setName("小李");
-        clickButton31.setType("click");
-
-        WechatButton button1 = new WechatButton();
-        button1.setName("小李软件");
-        button1.setSub_button(new WechatButton[]{clickButton11,clickButton12});
-
-        WechatButton button2 = new WechatButton();
-        button2.setName("软件商城");
-        button2.setSub_button(new WechatButton[]{viewButton21, viewButton22});
-
-        WechatButton button3 = new WechatButton();
-        button3.setName("移动端");
-        button3.setSub_button(new WechatButton[]{viewButton31, viewButton32});
-
-        menu.setButton(new WechatButton[]{button1,button2,button3});
-        return menu;
-    }
-
-    public static int createMenu(String token,String menu) {
-
-        int result = 0;
-        String url = CREATE_MENU_URL.replace("ACCESS_TOKEN", token);
-        JSONObject jsonObject = WechatUtil.doPost(url, JSONObject.parseObject(menu));
-        if(jsonObject != null){
-            System.out.println(jsonObject);
-            result = jsonObject.getIntValue("errcode");
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-
-        String token = WechatUtil.getAccessToken();
-        System.out.println("Access_Token为:"+token);
-        String menu = JSONObject.toJSONString(initMenu());
-        System.out.println(menu);
-        int result = createMenu(token, menu);
-        if(result==0){
-            System.out.println("菜单创建成功！");
-        }else{
-            System.out.println("菜单创建失败");
-        }
-    }
-}
+//package com.sl.demo.server.common;
+//
+//import com.alibaba.fastjson.JSONObject;
+//import com.sl.domain.dto.wechat.WechatButton;
+//import com.sl.domain.dto.wechat.WechatClickButton;
+//import com.sl.domain.dto.wechat.WechatMenu;
+//import com.sl.domain.dto.wechat.WechatViewButton;
+//
+//public class WechatMenuInit {
+//
+//    public static final String CREATE_MENU_URL = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
+//
+//    public static WechatMenu initMenu(){
+//        WechatMenu menu = new WechatMenu();
+//        WechatClickButton clickButton11 = new WechatClickButton();
+//        clickButton11.setKey("11");
+//        clickButton11.setName("关于我们");
+//        clickButton11.setType("click");
+//
+//        WechatClickButton clickButton12 = new WechatClickButton();
+//        clickButton12.setKey("12");
+//        clickButton12.setName("加入我们");
+//        clickButton12.setType("click");
+//
+//        WechatViewButton viewButton21 = new WechatViewButton();
+//        viewButton21.setUrl("http://www.qifou.net/test/test");
+//        viewButton21.setName("网站系统");
+//        viewButton21.setType("view");
+//
+//        WechatViewButton viewButton22 = new WechatViewButton();
+//        viewButton22.setUrl("http://www.qifou.net/test/test");
+//        viewButton22.setName("企业ERP");
+//        viewButton22.setType("view");
+//
+//        WechatViewButton viewButton31 = new WechatViewButton();
+//        viewButton31.setUrl("http://www.qifou.net/test/test");
+//        viewButton31.setName("APP");
+//        viewButton31.setType("view");
+//
+//        WechatViewButton viewButton32 = new WechatViewButton();
+//        viewButton32.setUrl("http://www.qifou.net/test/test");
+//        viewButton32.setName("微信小程序");
+//        viewButton32.setType("view");
+//
+//        WechatClickButton clickButton31 = new WechatClickButton();
+//        clickButton31.setKey("31");
+//        clickButton31.setName("小李");
+//        clickButton31.setType("click");
+//
+//        WechatButton button1 = new WechatButton();
+//        button1.setName("小李软件");
+//        button1.setSub_button(new WechatButton[]{clickButton11,clickButton12});
+//
+//        WechatButton button2 = new WechatButton();
+//        button2.setName("软件商城");
+//        button2.setSub_button(new WechatButton[]{viewButton21, viewButton22});
+//
+//        WechatButton button3 = new WechatButton();
+//        button3.setName("移动端");
+//        button3.setSub_button(new WechatButton[]{viewButton31, viewButton32});
+//
+//        menu.setButton(new WechatButton[]{button1,button2,button3});
+//        return menu;
+//    }
+//
+//    public static int createMenu(String token,String menu) {
+//
+//        int result = 0;
+//        String url = CREATE_MENU_URL.replace("ACCESS_TOKEN", token);
+//        JSONObject jsonObject = WechatUtil.doPost(url, JSONObject.parseObject(menu));
+//        if(jsonObject != null){
+//            System.out.println(jsonObject);
+//            result = jsonObject.getIntValue("errcode");
+//        }
+//        return result;
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        String token = WechatUtil.getAccessToken();
+//        System.out.println("Access_Token为:"+token);
+//        String menu = JSONObject.toJSONString(initMenu());
+//        System.out.println(menu);
+//        int result = createMenu(token, menu);
+//        if(result==0){
+//            System.out.println("菜单创建成功！");
+//        }else{
+//            System.out.println("菜单创建失败");
+//        }
+//    }
+//}
