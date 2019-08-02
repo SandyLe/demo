bf0ad92a276829984428a7575a1d5742f84047a1
lixianting <li_xianting@163.com>
2019-08-02 18:37:36 +0800

    用户保存接口

--- demo-server/src/main/java/com/sl/demo/server/controller/LoginController.java
+++ demo-server/src/main/java/com/sl/demo/server/controller/LoginController.java
@@ -3,10 +3,12 @@ package com.sl.demo.server.controller;
 
 import com.sl.demo.server.service.UserService;
 import com.sl.demo.server.util.LoginUtils;
+import com.sl.domain.dto.sys.LoginUser;
 import com.sl.domain.dto.sys.UserLoginDto;
 import com.sl.domain.dto.util.Result;
 import com.sl.domain.entity.User;
 import io.swagger.annotations.Api;
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
@@ -21,12 +23,15 @@ public class LoginController {
     @Autowired
     private UserService userService;
     @PostMapping(value = {"/login"})
-    public Result login(@RequestBody UserLoginDto userLoginDto){
+    public Result<LoginUser> login(@RequestBody UserLoginDto userLoginDto) throws Exception{
         UsernamePasswordToken token = new UsernamePasswordToken(userLoginDto.getUsername(),userLoginDto.getPassword());
         Subject subject = SecurityUtils.getSubject();
         subject.login(token);
         User user = userService.findByName(userLoginDto.getUsername());
-        LoginUtils.getSession().setAttribute(LoginUtils.LOGIN_USER, user);
-        return new Result();
+        LoginUser loginUser = new LoginUser();
+        BeanUtils.copyProperties(loginUser, user);
+        loginUser.setToken(SecurityUtils.getSubject().getSession().getId().toString());
+        LoginUtils.getSession().setAttribute(LoginUtils.LOGIN_USER, loginUser);
+        return new Result<LoginUser>(loginUser);
     }
 }
--- demo-server/src/main/java/com/sl/demo/server/util/LoginUtils.java
+++ demo-server/src/main/java/com/sl/demo/server/util/LoginUtils.java
@@ -1,5 +1,6 @@
 package com.sl.demo.server.util;
 
+import com.sl.domain.dto.sys.LoginUser;
 import com.sl.domain.entity.User;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.session.Session;
@@ -8,13 +9,17 @@ public class LoginUtils {
 
     public static final String LOGIN_USER = "com.sl.demo.server.login.user";
 
-    public static User getLoginUser(){
-        User user = (User) SecurityUtils.getSubject().getSession().getAttribute(LOGIN_USER);
+    public static LoginUser getLoginUser(){
+        LoginUser user = (LoginUser) SecurityUtils.getSubject().getSession().getAttribute(LOGIN_USER);
         return  user;
     }
 
     public static Session getSession(){
         Session session = SecurityUtils.getSubject().getSession();
         return session;
+    }
+
+    public static String getSessionId(){
+        return getSession().getId().toString();
     }
 }
