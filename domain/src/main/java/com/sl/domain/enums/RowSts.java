9154c622b01757f5ef263f573db4e3e0dc6b708d
lixianting <li_xianting@163.com>
2019-08-26 07:38:33 +0800

    RowSts枚举

---
+++ domain/src/main/java/com/sl/domain/enums/RowSts.java
@@ -0,0 +1,34 @@
+package com.sl.domain.enums;
+
+import io.swagger.annotations.ApiModel;
+
+@ApiModel(value = "RowSts", description = "枚举：状态")
+public enum RowSts {
+
+    NEW(5,"new"),//新增
+    EFFECTIVE(10,"effective"),//有效
+    DELETE(0,"delete");//删除
+    private Integer id;
+    private String name;
+
+    RowSts(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
