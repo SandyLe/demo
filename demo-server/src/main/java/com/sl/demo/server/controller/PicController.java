1bb02a03369157efd0544846eca8421c7a1c2efd
lixianting <li_xianting@163.com>
2019-09-04 11:05:21 +0800

    图片上传

---
+++ demo-server/src/main/java/com/sl/demo/server/controller/PicController.java
@@ -0,0 +1,50 @@
+package com.sl.demo.server.controller;
+
+import com.sl.demo.server.service.PositionService;
+import com.sl.demo.server.service.UserService;
+import com.sl.domain.entity.Position;
+import com.sl.domain.entity.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@Api(value = "TestController", description = "测试接口")
+public class PicController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private PositionService positionService;
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @ApiOperation(value = "测试",notes = "Test")
+    @PostMapping(value = {"/pic/upload"})
+    public String upload(@RequestParam(required = true, value = "multipartFiles") MultipartFile[] multipartFiles, @RequestParam(required = false, value = "albumCode")String albumCode) {
+
+        if(null != multipartFiles && multipartFiles.length > 0){
+            for(MultipartFile multipartFile : multipartFiles){
+                File file = new File(uploadPath + multipartFile.getOriginalFilename());
+                try{
+                    multipartFile.transferTo(file);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return "hello word1!";
+    }
+}
